75c255a03cbceac7087ac56fa94b9f8a79f3a9e7
姚迎迎 <yao3060@gmail.com>
2018-12-03 02:45:23 +0800

    docker try include nginx php-fpm mysql redis

---
+++ README.md
@@ -0,0 +1,13 @@
+# docker-test
+
+## Aliyun Sources
+```
+deb http://mirrors.aliyun.com/debian/ stretch main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ stretch main non-free contrib
+deb http://mirrors.aliyun.com/debian-security stretch/updates main
+deb-src http://mirrors.aliyun.com/debian-security stretch/updates main
+deb http://mirrors.aliyun.com/debian/ stretch-updates main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ stretch-updates main non-free contrib
+deb http://mirrors.aliyun.com/debian/ stretch-backports main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ stretch-backports main non-free contrib
+```
---
+++ app/phpinfo.php
@@ -0,0 +1,28 @@
+<?php
+
+// phpinfo();die;
+
+$redis = new \Redis();
+try {
+    $redis->connect('redis', 6379);
+    var_dump($redis);
+} catch (\Exception $e) {
+    var_dump($e->getMessage())  ;
+    die;
+}
+
+
+$link = mysqli_connect("mysql", "app", "password", "database");
+
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
